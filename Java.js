acc142ebbdd870b11632d098c64e95fd6a37f162
Devon Gildark <devonkg@fullsail.edu>
2013-08-12 22:29:02 -0700

    Commit 1

---
+++ Java.js
@@ -0,0 +1,27 @@
+// Devon Gildark 1308
+
+/*
+ *Meal vs Snack-- The meal list will be a auto count, so first meal is 1, second mealis 2 and so on. Will do the same for Snacks. The scroll bar will have
+  the choice of Meal or Snack, and at choice it will rememeber the difference and the count. Will also load a different screen.
+  If/Else set up. If Meal, Else Snack
+*/
+
+var Meal = true
+var Snack = false
+
+    if (true < false) {
+        if (true) {
+            //Load Meal Page
+        } else{
+            false //load Snack Page
+        }
+    }
+    
+// Need to make the code to read & save user names & passwords
+
+
+// code to save, edit and delete meals/snacks
+
+// code that ads all the macros together giving total cal count
+
+// code to total cal count show up next to slide bar
