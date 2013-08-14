1cca8efa1cb427c575086b350c104315b8ee296b
Devon Gildark <devonkg@fullsail.edu>
2013-08-13 23:16:09 -0700

    Commitn2

--- Java.js
+++ Java.js
@@ -19,9 +19,12 @@ var Snack = false
     
 // Need to make the code to read & save user names & passwords
 
+// Meal page is hidden until selected, if snack is chosen then it goes to the snack page.
 
 // code to save, edit and delete meals/snacks
 
 // code that ads all the macros together giving total cal count
 
 // code to total cal count show up next to slide bar
+
+// once logged takes you to a page showing a lift of all the logged food
