d5016d092ab833c46ba265afe9859a8dae245b47
Devon Gildark <devonkg@fullsail.edu>
2013-08-15 00:26:14 -0700

    commit 6

--- Java.js
+++ Java.js
@@ -1,21 +1,13 @@
 // Devon Gildark 1308
 
+window.addEventListener("DOMcontentloaded", function(){
+
 /*
  *Meal vs Snack-- The meal list will be a auto count, so first meal is 1, second mealis 2 and so on. Will do the same for Snacks. The scroll bar will have
   the choice of Meal or Snack, and at choice it will rememeber the difference and the count. Will also load a different screen.
   If/Else set up. If Meal, Else Snack
 */
 
-var meal = document.getElementById("meal")
-//var snack = document.getElementById("snack")
-var loaded = document.getElementById("loaded")
-
-    if (meal) {
-            //Meal Page
-        } else{
-            loaded //loaded Page
-        }
-    
 // Need to make the code to read & save user names & passwords
 
 // Meal page is hidden until selected, if snack is chosen then it goes to the snack page.
@@ -27,3 +19,6 @@ var loaded = document.getElementById("loaded")
 // code to total cal count show up next to slide bar
 
 // once logged takes you to a page showing a lift of all the logged food....
+
+
+});
