f55397074de328534adc64820e6104a3622e24d4
Devon Gildark <devonkg@fullsail.edu>
2013-08-15 11:19:28 -0700

    Commit7

--- Java.js
+++ Java.js
@@ -2,6 +2,34 @@
 
 window.addEventListener("DOMcontentloaded", function(){
 
+// DOM Shortcuts
+var dId = function (x){
+	var elId = document.getElementById(x);
+	return elId;
+};
+var dTag = function (x){
+	var elTag = document.getElementsByTagName(x);
+	return elTag;
+};	
+var dCreate = function (x){
+	var elCreate = document.createElement(x);
+	return elCreate;
+};
+// End DOM Shortcuts
+
+//Var defaults- Going of the video set up
+var pageGroups= ["--create group--", "Meal", "Snack", "Loaded"];
+
+//Links & Submit click evcents
+var edit = dId("edit");
+    edit.addEventListener("click", getData);
+
+var remove = dId("delete");
+    remove.addEventListener("click", clearLocal);
+
+var save = dId("submit");
+    save.addEventListener("click", storeData);
+
 /*
  *Meal vs Snack-- The meal list will be a auto count, so first meal is 1, second mealis 2 and so on. Will do the same for Snacks. The scroll bar will have
   the choice of Meal or Snack, and at choice it will rememeber the difference and the count. Will also load a different screen.
